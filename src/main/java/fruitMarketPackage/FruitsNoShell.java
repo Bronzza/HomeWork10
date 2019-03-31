6e1b556e574a17eab79022468af0e52a9b570487
Sergey Bronzza <repinetskiy@gmail.com>
2019-03-31 16:57:16 +0300

    Commit to secondBranch Enum added to git

---
+++ src/main/java/fruitMarketPackage/FruitsNoShell.java
@@ -0,0 +1,12 @@
+package fruitMarketPackage;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+//@JsonSerialize (using = FruitNoShellSerializator.class)
+public enum FruitsNoShell {
+    STRAWBERRY ,
+    APPLE,
+    BANANA,
+    PEAR,
+    PINEAPPLE;
+}
